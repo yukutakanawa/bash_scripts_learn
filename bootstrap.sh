afc155424ae0005d94977e18ec9e40c614b6b213
yukutakanawa <yukutana>
2026-02-13 20:57:54 +0300

    edit .git file

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,12 +18,15 @@ echo "===================================================="
 # Настраиваем git добавляем данные от git
 echo "Настраиваем git добавляем user и почту"
 
-if [ -z "$1" ] && [ -z "$2" ]; then 
-	USER_NAME="yukutakanawa"
-	USER_EMAIL="yukutana"
-else
+if [ -n "$1" ] && [ -n "$2" ]; then 
 	USER_NAME="$1"
 	USER_EMAIL="$2"
+elif [ -z "$1" ] && [ -z "$2"]; then
+	USER_NAME="yukutakanawa"
+	USER_EMAIL="yukutakanawa@gmail.com"
+else 
+	echo "Ошибка: нужно передать ИМЯ и EMAIL вместе, либо не передавать ничего"
+	exit 1
 fi
 
 git config --global user.name "$USER_NAME"
